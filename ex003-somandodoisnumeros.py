8c68d216d200addc5a03bd3f8b3b83142d56fd73
Victor Casarin Antunes <112783534+victorcasarin@users.noreply.github.com>
2024-08-29 08:13:28 -0300

    Add files via upload

---
+++ ex003-somandodoisnumeros.py
@@ -0,0 +1,12 @@
+#Crie um programa que leia dois números e mostre a soma entre eles.
+
+#definindo as varáiveis
+n1 = float(input('Digite um valor: '))
+n2 = float(input('Digite outro valor: '))
+
+#somando
+s = n1 + n2
+
+#Resultado
+print(f'A soma entre {n1} e {n2} é igual a:{s:.1f}') #imprime o valor de s formatado com duas casas decimais.
+
